9d990524c38f6b435135441908a953adb956dd47
Hiroshi Kajino <hero@beyond-a.com>
2024-01-12 15:33:29 +0900

    new init.el

--- init.el
+++ init.el
@@ -1,151 +1,120 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; <leaf-install-code>
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
 
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(add-to-list 'load-path "emacs-flycheck-mypy")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-        (eval-print-last-sexp)))
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
 
-;(el-get-bundle elpa:w3m)
-;(setq w3m-imagick-convert-program nil)
-
-
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
-
-(add-to-list 'display-buffer-alist
-	     '("^\\*shell\\*$" . (display-buffer-same-window)))
-
-
-;(eval-after-load 'company
-;  '(add-to-list 'company-backends 'company-irony))
-
-; magit
-;(el-get-bundle magit)
-
-; fish
-;(el-get-bundle emacs-fish)
-					;company mode
-(el-get-bundle company-mode/company-mode)
-(global-company-mode +1)
-;; 自動補完を offにしたい場合は, company-idle-delayを nilに設定する
-;; auto-completeでいうところの ac-auto-start にあたる.
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+;; </leaf-install-code>
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-idle-delay nil)
- '(package-selected-packages (quote (org-edna w3m))))
-(set-face-attribute 'company-tooltip nil
-		    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common nil
-		    :foreground "black" :background "lightgrey")
-(set-face-attribute 'company-tooltip-common-selection nil
-		    :foreground "white" :background "steelblue")
-(set-face-attribute 'company-tooltip-selection nil
-		    :foreground "black" :background "steelblue")
-(set-face-attribute 'company-preview-common nil
-		    :background nil :foreground "lightgrey" :underline t)
-(set-face-attribute 'company-scrollbar-fg nil
-		    :background "orange")
-(set-face-attribute 'company-scrollbar-bg nil
-		    :background "gray40")
-(global-set-key (kbd "C-M-i") 'company-complete)
-
-;; C-n, C-pで補完候補を次/前の候補を選択
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-search-map (kbd "C-n") 'company-select-next)
-(define-key company-search-map (kbd "C-p") 'company-select-previous)
-
-;; C-sで絞り込む
-(define-key company-active-map (kbd "C-s") 'company-filter-candidates)
-
-;; TABで候補を設定
-(define-key company-active-map (kbd "C-i") 'company-complete-selection)
-
-;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
-(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
-
-					; jedi
-(el-get-bundle auto-complete)
-;(el-get-bundle company-jedi)
-(el-get-bundle jedi-core)
-(require 'jedi-core)
-(setq jedi:complete-on-dot t)
-(setq jedi:use-shortcuts t)
-(add-hook 'python-mode-hook 'jedi:setup)
-;(add-hook 'python-mode-hook 'company-jedi)
-(add-to-list 'company-backends 'company-jedi)
-;(require 'jedi-core)
-;(setq jedi:complete-on-dot t)
-;(setq jedi:use-shortcuts t)
-;(add-hook 'python-mode-hook 'jedi:setup)
-
-; pep8
-(el-get-bundle python-pep8)
-
-;(el-get-bundle company-jedi)
-;(el-get-bundle jedi-core)
-;(el-get-bundle jedi)
-;(require 'jedi-core)
-;(setq jedi:complete-on-dot t)
-;(setq jedi:use-shortcuts t)
-;(add-hook 'python-mode-hook 'jedi:setup)
-					;(add-to-list 'company-backends 'company-jedi) ; backendに追加
-(setq jedi:get-in-function-call-delay 0)
-
-; yatex
-(el-get-bundle yatex :type hg :url "http://www.yatex.org/hgrepos/yatex" :branch "dev")
-(setq auto-mode-alist
-      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-(setq tex-command "pdflatex -synctex=1")
-(setq tex-pdfview-command "open -a Preview")
-(setq dvi2-command "open -a Preview")
-
-; reftex
-(el-get-bundle reftex)
-(add-hook 'yatex-mode-hook 'turn-on-reftex)
-(setq reftex-label-alist '((nil ?e nil "~\\eqref{%s}" nil nil)))
-
-; template
-(setq auto-insert-directory "~/.emacs.d/template/")
-(setq auto-insert-query nil)
-(define-auto-insert "\\.py\\'" "template.py")
-(define-auto-insert "\\.sh\\'" "template.sh")
-(add-hook 'find-file-hooks 'auto-insert)
+ '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
-; aspell
-(setq-default ispell-program-name "aspell")
-(eval-after-load "ispell"
-   '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+;; flycheck
+(leaf flycheck
+  :doc "On-the-fly syntax checking"
+  :emacs>= 24.3
+  :ensure t
+  :bind (("M-n" . flycheck-next-error)
+         ("M-p" . flycheck-previous-error))
+  :custom ((flycheck-emacs-lisp-initialize-packages . t)
+	   (flycheck-display-errors-delay . 0.3)
+	   (flycheck-indication-mode . 'left-margin)) ;terminalで使うので、fringeではなくmarginに警告を表示
+  :hook (emacs-lisp-mode-hook lisp-interaction-mode-hook prog-mode-hook . flycheck-mode)
+  :config
+  (leaf flycheck-package
+    :doc "A Flycheck checker for elisp package authors"
+    :ensure t
+    :config
+    (flycheck-package-setup))
+  (leaf flycheck-elsa
+    :doc "Flycheck for Elsa."
+    :emacs>= 25
+    :ensure t
+    :config
+    (flycheck-elsa-setup))
+  (leaf flycheck-inline
+    :ensure t
+    :hook (flycheck-mode-hook . flycheck-inline-mode))
+  (add-hook 'flycheck-mode-hook #'flycheck-set-indication-mode) ; flycheckのみでmarginを使用
+)
+					;
 
-; flyspell
-(el-get-bundle flyspell)
-(add-hook 'yatex-mode-hook 'flyspell-mode)
-;(mapc
-; (lambda (hook)
-;   (add-hook hook
-;	     '(lambda () (flyspell-mode 1))))
-; '(
-;   yatex-mode-hook
-;      ))
+; parenthese
+(leaf smartparens
+  :ensure t
+  :hook (after-init-hook . smartparens-global-strict-mode) ; strictモードを有効化
+  :require smartparens-config
+  :custom ((electric-pair-mode . nil))) ; electirc-pair-modeを無効化
 
-; hs-headline
-(add-hook 'python-mode-hook '(lambda () (hs-minor-mode 1)))     
+(leaf rainbow-delimiters
+  :ensure t
+  :hook
+  ((prog-mode-hook       . rainbow-delimiters-mode)))
 
-; quickrun
-(el-get-bundle quickrun)
-(global-set-key (kbd "<f5>") 'quickrun)
+					; python
+(leaf py-isort :ensure t)
+
+					; python completion
+
+(leaf company
+  :ensure t
+  :leaf-defer nil
+  :blackout company-mode
+  :bind ((company-active-map
+          ("M-n" . nil)
+          ("M-p" . nil)
+          ("C-s" . company-filter-candidates)
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)
+          ("C-i" . company-complete-selection))
+         (company-search-map
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)))
+  :custom ((company-tooltip-limit         . 12)
+           (company-idle-delay            . 0) ;; 補完の遅延なし
+           (company-minimum-prefix-length . 1) ;; 1文字から補完開始
+           (company-transformers          . '(company-sort-by-occurrence))
+           (global-company-mode           . t)
+           (company-selection-wrap-around . t))
+  )
+
+					; magit
+(leaf magit
+  :bind (("C-x g" . magit-status))
+  :ensure t
+  :custom
+  `((magit-completing-read-function . 'magit-builtin-completing-read)
+    (magit-refs-show-commit-count   . 'all)
+    (magit-log-buffer-file-locked   . t)
+    (magit-revision-show-gravatars  . nil)
+    )
+  )
 
 ; divide display into three
 (defun split-window-vertically-n (num_wins)
@@ -170,119 +139,9 @@
 (global-set-key "\C-x#" '(lambda ()
 			   (interactive)
 			   (split-window-horizontally-n 3)))
+
 ; window move
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
-
-; auto completion
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (define-key python-mode-map "\"" 'electric-pair)
-	    (define-key python-mode-map "\'" 'electric-pair)
-	    (define-key python-mode-map "(" 'electric-pair)
-	    (define-key python-mode-map "[" 'electric-pair)
-	    (define-key python-mode-map "{" 'electric-pair)))
-(defun electric-pair ()
-  "Insert character pair without sournding spaces"
-  (interactive)
-  (let (parens-require-spaces)
-    (insert-pair)))
-
-					; markdown
-;(add-to-list 'exec-path "/usr/local/opt/w3m/bin/")
-;(el-get-bundle elpa:w3m)
-(el-get-bundle markdown-mode)
-(require 'markdown-mode)
-;(require 'w3m)
-;(define-key markdown-mode-map (kbd "\C-c \C-c \C-v")
-;  (lambda ()
-;    (interactive)
-;    (setq html-file-name (concat (file-name-sans-extension (buffer-file-name)) ".html"))
-;    (markdown-export html-file-name)
-;    (if (one-window-p) (split-window))
-;    (other-window 1)
-;    (w3m-find-file html-file-name)))
-
-;(require 'markdown-mode)
-;(require 'w3m)
-;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-;(define-key markdown-mode-map "\C-c\C-cm"
-;  (lambda ()
-;    (interactive)
-;    (w3m-find-file (buffer-file-name))))
-
-(add-hook 'python-mode-hook
-	  '(lambda ()
-	     (hs-minor-mode 1)))
-(define-key global-map (kbd "C-\\") 'hs-toggle-hiding)
-
-; cython
-(el-get-bundle cython-mode)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
-;;; racerやrustfmt、コンパイラにパスを通す
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-;;; rust-modeでrust-format-on-saveをtにすると自動でrustfmtが走る
-(eval-after-load "rust-mode"
-  '(setq-default rust-format-on-save t))
-;;; rustのファイルを編集するときにracerとflycheckを起動する
-(add-hook 'rust-mode-hook (lambda ()
-                            (racer-mode)
-                            (flycheck-rust-setup)))
-;;; racerのeldocサポートを使う
-(add-hook 'racer-mode-hook #'eldoc-mode)
-;;; racerの補完サポートを使う
-(add-hook 'racer-mode-hook (lambda ()
-                             (company-mode)
-                             ;;; この辺の設定はお好みで
-                             (set (make-variable-buffer-local 'company-idle-delay) 0.1)
-                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 0)))
-(el-get-bundle flycheck)
-;(require 'flycheck-mypy)
-(require 'flycheck)
-(flycheck-define-checker
-    python-mypy ""
-    :command ("mypy"
-              "--ignore-missing-imports" "--fast-parser"
-              "--python-version" "3.6"
-              source-original)
-    :error-patterns
-    ((error line-start (file-name) ":" line ": error:" (message) line-end))
-    :modes python-mode)
-
-(add-to-list 'flycheck-checkers 'python-mypy t)
-(flycheck-add-next-checker 'python-pylint 'python-mypy t)
-(add-hook 'python-mode-hook
-	  '(lambda ()
-	     (flycheck-mode 1)))
-
-(el-get-bundle ein)
-(require 'ein)
-
-;; rainbow-delimiters を使うための設定
-(el-get-bundle rainbow-delimiters)
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-
-;; 括弧の色を強調する設定
-(require 'cl-lib)
-(require 'color)
-(defun rainbow-delimiters-using-stronger-colors ()
-  (interactive)
-  (cl-loop
-   for index from 1 to rainbow-delimiters-max-face-count
-   do
-   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
-    (cl-callf color-saturate-name (face-foreground face) 30))))
-(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
-
-(package-initialize)
